fac2ccbc6a7f0abedaf759be5e9bcb61187bc06c
Chris Zieba <zieba.chris@gmail.com>
2015-03-27 20:40:57 -0700

    Use single quotes.

--- src/Game.js
+++ src/Game.js
@@ -13,8 +13,8 @@ var Blackjack = Blackjack || {};
 
 Blackjack.Game = (function () {
 
-    var SUITS = ["♥", "♦", "♠", "♣"];
-    var RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    var SUITS = ['♥', '♦', '♠', '♣'];
+    var RANKS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
     
     // constructor
