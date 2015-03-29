3d63126b86175c032b26d8e9f0dafdc2396480d2
Chris Zieba <zieba.chris@gmail.com>
2015-03-29 12:08:24 -0700

    Cleanup the hit function

--- src/Probability.js
+++ src/Probability.js
@@ -80,16 +80,38 @@ Blackjack.Probability = (function() {
         return stats;
     };
 
-
+    /**
+     * Calculate the win/losss/push probabilities of the player hitting their hand.
+     *
+     * @param {Array} shoe
+     * @param {Object} dealerCards
+     * @param {Object} playerCards
+     * @param {Integer} maxPullCount
+     * @param {Integer} pullCount
+     * @param {Integer} startIndex
+     * @return {Object} stats
+     */
     var hit = function(shoe, dealerCards, playerCards, maxPullCount, pullCount, startIndex) {
         var playerTotalAfterPull, playerCardsAfterPull, newStats;
         var playerTotal = Blackjack.Utils.score(playerCards);
         var stats = initStats();
 
-        for (var i = 0, j=pullCount+startIndex; i < shoe.length-pullCount; i+=1, j+=1) {
+        /**
+         * Loop through every card in the shoe.
+         * Use the counters i,j, and pullCount to remember at what 
+         * point in the shoe we are.
+         */
+        for (var i = 0, j = pullCount + startIndex; i < shoe.length - pullCount; i+=1, j+=1) {
+            // The player pulls a card from the shoe
             j = (j >= shoe.length) ? 0 : j;
+
+            // This is just making sure we wrap around the array
             var nextCard = shoe[(pullCount > 0) ? j : i];
 
+            /**
+             * Calculate the probability of pulling a card from the deck.
+             * The probability for pulling one card from the shoe is 1/(cards left in shoe).
+             */
             var cardProbabilty = 1;
             for (var l=0; l<=pullCount; l+=1) {
                 cardProbabilty*= 1/(shoe.length-l)
@@ -97,19 +119,30 @@ Blackjack.Probability = (function() {
 
             playerCardsAfterPull = playerCards.slice();
             playerCardsAfterPull.push(nextCard);
-
+            
+            // Only take a card if not a blackjack
             if (pullCount == 0 && playerTotal == 21) {
                 playerTotalAfterPull = playerTotal;
             } else {
                 playerTotalAfterPull = Blackjack.Utils.score(playerCardsAfterPull);
             }
 
+            // Even if the player total is a bust we need run through the hand probabilites agasint the dealer 
             if (playerTotalAfterPull >= 17) {
-                newStats = stand(shoe, dealerCards, playerCardsAfterPull, maxPullCount, pullCount+1, 0);
+                /**
+                 * If the player hand is within standing range (17 and 21) or bust (> 21)
+                 * we need to calculate the probabilites as if the hand was played out by the dealer.
+                 */
+                newStats = stand(shoe, dealerCards, playerCardsAfterPull, maxPullCount, pullCount + 1, 0);
                 stats = combineStats(stats, newStats);
             } else {
+                /**
+                 * If the player hand is not withing the standing range we hit again.
+                 * The startIndex is important because we want to pull a card at a 
+                 * specific element in the shoe (the next card).
+                 */
                 if (pullCount <= maxPullCount) {
-                    newStats = hit(shoe, dealerCards, playerCardsAfterPull, maxPullCount, pullCount+1, (pullCount > 0) ? startIndex : i);
+                    newStats = hit(shoe, dealerCards, playerCardsAfterPull, maxPullCount, pullCount + 1, (pullCount > 0) ? startIndex : i);
                     stats = combineStats(stats, newStats);
                 }
             }
