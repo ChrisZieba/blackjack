77f84a0a13a125554eec0ce8425b743dd59ec7b9
Chris Zieba <zieba.chris@gmail.com>
2015-03-29 22:42:19 -0700

    Convert tabs to spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,39 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		jshint: {
-			files: [
-				'src/**/*.js'
-			],
-			options: {
-				ignores: [],
-				curly: true,
-				eqnull: true,
-				browser: true,
-				globals: {
-					jQuery: true,
-					console: true,
-					exports: true,
-					require: true
-				},
-			}
-		},
-		uglify: {
-			options: {
-				banner: '/*! <%= pkg.name %> <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
-				mangle: false
-			},
-			build: {
-				files: {
-					'dist/blackjack-<%= pkg.version %>.min.js': ['src/**/*.js']
-				}
-			},
-		}
-	});
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            files: [
+                'src/**/*.js'
+            ],
+            options: {
+                ignores: [],
+                curly: true,
+                eqnull: true,
+                browser: true,
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    exports: true,
+                    require: true
+                },
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                mangle: false
+            },
+            build: {
+                files: {
+                    'dist/blackjack-<%= pkg.version %>.min.js': ['src/**/*.js']
+                }
+            },
+        }
+    });
 
 
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-	grunt.registerTask('default', ['jshint', 'uglify']);
+    grunt.registerTask('default', ['jshint', 'uglify']);
 };
